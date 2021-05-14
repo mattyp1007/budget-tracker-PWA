41915ee4164a2ede1bfe489b63ee7256c4df336a
Matthew Parker <matt.parker1007@gmail.com>
2021-05-14 17:33:26 -0500

    mongoose db uri

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
